3dc916e79f1b7a74bb1c00d3e654718146d31ef3
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-22 18:52:13 -0300

    dotenv adicionado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import 'express-async-errors';
 import path from 'path';
 import cors from 'cors';
+import * as dotenv from 'dotenv';
 import './database/connection';
 import errorHandler from './errors/handler';
 import routes from './routes';
 
+dotenv.config({ path: __dirname + '/.env' });
 const app = express();
 
 
