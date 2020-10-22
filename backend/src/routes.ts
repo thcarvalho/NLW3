a8017dfae7a74ffc63850b9717ecf88a37f3057d
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-22 18:52:02 -0300

    rota de autenticação e de usuários

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import AuthController from './controllers/AuthController';
 
 import OrphanageController from './controllers/OrphanageController';
+import UserController from './controllers/UserController';
 import uploadImages from './middlewares/upload-images';
 const routes = Router();
 
@@ -8,5 +10,11 @@ const routes = Router();
 routes.post('/orphanages', uploadImages.array('images'), OrphanageController.create);
 routes.get('/orphanages', OrphanageController.index);
 routes.get('/orphanages/:id', OrphanageController.show);
+
+routes.post('/users', UserController.create);
+routes.get('/users', UserController.index)
+routes.get('/users/:id', UserController.show)
+
+routes.post('/auth', AuthController.create)
 
 export default routes;
