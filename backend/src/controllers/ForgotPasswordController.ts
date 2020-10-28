8b78e1816de33ec9081aaff229653bd23bec3a6f
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-27 23:34:07 -0300

    controller para envio de email sobre esquecimento de senha

---
+++ backend/src/controllers/ForgotPasswordController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+
+export default {
+  async create(req: Request, res: Response) {
+    
+  }
+}
