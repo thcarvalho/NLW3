9ba7a897e8ea6812b100f412ac285854bcb1dea9
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-27 23:12:01 -0300

    função do token atualizada

--- backend/src/middlewares/auth-token.ts
+++ backend/src/middlewares/auth-token.ts
@@ -2,12 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
 export default function (req: Request, res: Response, next: NextFunction) {
-  const token = <string>req.headers['authorization'];
+  const token = <string>req.headers.authorization;
   if (!token) {
-    res.status(401).send({error: 'no-token-provided'});
+    return res.status(401).send({error: 'no-token-provided'});
   }
 
-  const verified = jwt.verify(token, process.env.JWT_TOKEN as string)
+  const verified = jwt.verify(token, process.env.JWT_SECRET as string)
 
   res.locals.jwtPayload = verified
 
