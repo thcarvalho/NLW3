b7200f9df37e05be7741efbb2d3f39b36bab4e06
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-10-27 23:33:38 -0300

    controller de reset de senha

---
+++ backend/src/controllers/ResetPasswordController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import User from '../models/User';
+import bcrypt from 'bcrypt';
+
+export default {
+  async update(req: Request, res: Response) {
+    
+  }
+}
