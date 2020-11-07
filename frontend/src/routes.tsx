db268a8297d452c3fc3d899b726ed117983e8700
Thaian Carvalho <42048829+thcarvalho@users.noreply.github.com>
2020-11-07 19:44:41 -0300

    rota de reset password criada

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -6,6 +6,7 @@ import ForgotPassword from './pages/ForgotPassword';
 import Landing from './pages/Landing';
 import Orphanage from './pages/Orphanage';
 import OrphanagesMap from './pages/OrphanagesMap';
+import ResetPassword from './pages/ResetPassword';
 import RestrictAccessLogin from './pages/RestrictAccessLogin';
 
 export default function Routes() {
@@ -20,6 +21,7 @@ export default function Routes() {
 
         <Route path="/access" component={RestrictAccessLogin} />
         <Route path="/forgotpassword" component={ForgotPassword} />
+        <Route path="/resetpassword" component={ResetPassword} />
       </Switch>
     </BrowserRouter>
   );
